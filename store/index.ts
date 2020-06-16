f06d6f3ca0e6ab456f4c6e2d2244dfb79036baac
lee-leonardo <lee.leonardo.king@gmail.com>
2020-06-16 14:37:40 -0700

    whitespace change + adding modules and states

--- store/index.ts
+++ store/index.ts
@@ -1,32 +1,36 @@
-import * as root from './root';
-import * as generator from './modules/generator';
-import * as serviceworker from './modules/serviceworker';
-import * as publish from './modules/publish';
-import * as i18n from './modules/i18n';
-import * as windows from './modules/windows';
+import * as root from "./root";
+import * as generator from "./modules/generator";
+import * as serviceworker from "./modules/serviceworker";
+import * as push from "./modules/push";
+import * as publish from "./modules/publish";
+import * as i18n from "./modules/i18n";
+import * as windows from "./modules/windows";
+import { Push } from "typescript";
 
 // More info about store: https://vuex.vuejs.org/en/core-concepts.html
 // Structure of the store:
-    // Types: Types that represent the keys of the mutations to commit
-    // State: The information of our app, we can get or update it.
-    // Getters: Get complex information from state
-    // Action: Sync or async operations that commit mutations
-    // Mutations: Modify the state
+// Types: Types that represent the keys of the mutations to commit
+// State: The information of our app, we can get or update it.
+// Getters: Get complex information from state
+// Action: Sync or async operations that commit mutations
+// Mutations: Modify the state
 
 export const modules = {
-    [generator.name]: generator,
-    [serviceworker.name]: serviceworker,
-    [publish.name]: publish,
-    [i18n.name]: i18n,
-    [windows.name]: windows
+  [generator.name]: generator,
+  [serviceworker.name]: serviceworker,
+  [publish.name]: publish,
+  [push.name]: push,
+  [i18n.name]: i18n,
+  [windows.name]: windows,
 };
 
 interface ModulesStates {
-    generator: generator.State;
-    serviceworker: serviceworker.State;
-    publish: publish.State;
-    i18n: i18n.State;
-    windows: windows.State;
+  generator: generator.State;
+  serviceworker: serviceworker.State;
+  push: push.State;
+  publish: publish.State;
+  i18n: i18n.State;
+  windows: windows.State;
 }
 
 export type RootState = root.State & ModulesStates;
